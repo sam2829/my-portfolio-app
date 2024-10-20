45d9a6daea421248e97a24c169f75aa482e8570f
sam2829 <samscarisbrick@outlook.com>
2024-10-20 12:23:23 +0100

    Add allowed host to settings

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -46,9 +46,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [
-    'localhost', '127.0.0.1', 'sam-scarisbrick-13b7fd9648a6.herokuapp.com'
-    ]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST', '').split(',') + ['localhost', '127.0.0.1']
 
 
 # Application definition
