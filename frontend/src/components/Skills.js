9bf4acc42285fed9fb0d8871386e43cb1b28b3f4
sam2829 <samscarisbrick@outlook.com>
2024-11-01 17:56:07 +0000

    Add functionality to call to backend and retrieve technologies from backend in about page. Must still tidy up code and get motion framer to work on tech lists with backend data

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -3,40 +3,45 @@ import { motion } from "framer-motion";
 import styles from "../styles/Skills.module.css";
 
 //  component to render list of skills
-const Skills = ({ skills }) => {
-  const frontendSkills = [
-    "HTML",
-    "CSS",
-    "Bootstrap",
-    "Tailwind CSS",
-    "JavaScript",
-    "React.JS",
-  ];
+const Skills = ({ skillsData, skills }) => {
+  // Determine which skills list to render based on the prop value
+  const skillsList = skillsData[skills] || [];
+  const delay =
+    skills === "frontendSkills" ? 5 : skills === "backendSkills" ? 7 : 12;
 
-  const backendSkills = ["Python", "Django", "Django Rest"];
+  // const frontendSkills = [
+  //   "HTML",
+  //   "CSS",
+  //   "Bootstrap",
+  //   "Tailwind CSS",
+  //   "JavaScript",
+  //   "React.JS",
+  // ];
 
-  const toolsSkills = ["Git", "GitHub", "Heroku", "Cloudinary", "Cloudfare"];
+  // const backendSkills = ["Python", "Django", "Django Rest"];
+
+  // const toolsSkills = ["Git", "GitHub", "Heroku", "Cloudinary", "Cloudfare"];
 
   // Determine which skills list to render based on the prop value
-  let skillsList;
-  let delay = 0; // Default delay
+  // let skillsList;
+  // let delay = 0;
 
-  switch (skills) {
-    case "frontendSkills":
-      skillsList = frontendSkills;
-      delay = 1;
-      break;
-    case "backendSkills":
-      skillsList = backendSkills;
-      delay = 3;
-      break;
-    case "toolsSkills":
-      skillsList = toolsSkills;
-      delay = 4;
-      break;
-    default:
-      skillsList = [];
-  }
+  // switch (skills) {
+  //   case "frontendSkills":
+  //     skillsList = frontendSkills;
+  //     delay = 1;
+  //     break;
+  //   case "backendSkills":
+  //     skillsList = backendSkills;
+  //     delay = 3;
+  //     break;
+  //   case "toolsSkills":
+  //     skillsList = toolsSkills;
+  //     delay = 4;
+  //     break;
+  //   default:
+  //     skillsList = [];
+  // }
 
   return (
     <div>
@@ -52,20 +57,22 @@ const Skills = ({ skills }) => {
         }}
         className={styles.SkillsList}
       >
-        {skillsList.map((skill, index) => (
-          // use motion.li to create animation for li components
-          <motion.li
-            variants={{
-              hidden: { opacity: 0, scale: 0.5 },
-              visible: { opacity: 1, scale: [0.8, 1.3, 1] },
-            }}
-            transition={{ type: "spring" }}
-            className={styles.ListItems}
-            key={index}
-          >
-            {skill}
-          </motion.li>
-        ))}
+        {skillsList.length &&
+          skillsList.map((skill, index) => (
+            // use motion.li to create animation for li components
+            <motion.li
+              key={index}
+              variants={{
+                hidden: { opacity: 0, scale: 0.5 },
+                visible: { opacity: 1, scale: [0.8, 5, 1] },
+              }}
+              transition={{ type: "spring" }}
+              className={styles.ListItems}
+            >
+              {skill}
+            </motion.li>
+          ))}
+        {!skillsList.length && <p>No Skills....</p>}
       </motion.ul>
     </div>
   );
--- frontend/src/pages/About/AboutPage.js
+++ frontend/src/pages/About/AboutPage.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../styles/AboutPage.module.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Skills from "../../components/Skills";
 import AboutPageText from "./AboutPageText";
+import axios from "axios";
 
 const AboutPage = () => {
+  // State management for skills
+  const [skillsData, setSkillsData] = useState({
+    frontendSkills: [],
+    backendSkills: [],
+    toolsSkills: [],
+  });
+
+  // useEffect hook to fetch skills from api
+  useEffect(() => {
+    // Fetch the skills data from the API
+    const fetchSkills = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://127.0.0.1:8000/api/technologies/"
+        );
+        console.log("API response data:", data);
+
+        // Organize skills by skill_type
+        const organizedSkills = {
+          frontendSkills: data.results
+            .filter((skill) => skill.skill_type === "Frontend")
+            .map((skill) => skill.name),
+          backendSkills: data.results
+            .filter((skill) => skill.skill_type === "Backend")
+            .map((skill) => skill.name),
+          toolsSkills: data.results
+            .filter((skill) => skill.skill_type === "Tools")
+            .map((skill) => skill.name),
+        };
+
+        setSkillsData(organizedSkills);
+      } catch (error) {
+        console.error("Error fetching skills:", error);
+      }
+    };
+
+    fetchSkills();
+  }, []);
+
   return (
     <Container>
       <main className={styles.Main}>
@@ -27,7 +67,7 @@ const AboutPage = () => {
         <Row className="justify-content-center">
           {/* column for about me text */}
           <Col xs={12} lg={5} className={`${styles.TextColumn} mx-auto`}>
-          {/* Import About me page text */}
+            {/* Import About me page text */}
             <AboutPageText />
           </Col>
           <Col xs={12} lg={5} className="mx-auto">
@@ -37,13 +77,13 @@ const AboutPage = () => {
             </h3>
             <h6 className={styles.SkillsHeading}>Frontend:</h6>
             {/* important list of skills depending on prop passed */}
-            <Skills skills="frontendSkills" />
+            <Skills skillsData={skillsData} skills="frontendSkills" />
             <h6 className={styles.SkillsHeading}>Backend:</h6>
             {/* important list of skills depending on prop passed */}
-            <Skills skills="backendSkills" />
+            <Skills skillsData={skillsData} skills="backendSkills" />
             <h6 className={styles.SkillsHeading}>Tools:</h6>
             {/* important list of skills depending on prop passed */}
-            <Skills skills="toolsSkills" />
+            <Skills skillsData={skillsData} skills="toolsSkills" />
           </Col>
         </Row>
       </main>
