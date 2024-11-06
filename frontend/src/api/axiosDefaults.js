1ed19ecb794947efd53d3091dde64f70da3abfd5
sam2829 <samscarisbrick@outlook.com>
2024-11-06 14:40:18 +0000

    Change api requests to https custom domain

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://sam-scarisbrick-13b7fd9648a6.herokuapp.com',
+    baseURL: 'https://samscarisbrick.co.uk',
     withCredentials: true,
 });
 
