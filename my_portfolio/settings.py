f05f900226492cdd787965adddebcf1032b5d034
sam2829 <samscarisbrick@outlook.com>
2024-11-09 08:17:29 +0000

    Add www. to handle submit url for email

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -105,14 +105,6 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_METHODS = [
-    'GET',
-    'POST',
-    'PUT',
-    'DELETE',
-    'OPTIONS',
-]
-
 ROOT_URLCONF = 'my_portfolio.urls'
 
 TEMPLATES = [
